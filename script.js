9bd8c7681864282e333b7bfe4465f785274ee86b
galinatikhonskaya-eng <galinatikhonskaya@gmail.com>
2026-02-14 22:08:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ tg.expand();
 
 function openMenu() {
   document.getElementById("sideMenu").classList.add("active");
+  document.querySelector(".overlay").classList.add("active");
 }
 
 function closeMenu() {
   document.getElementById("sideMenu").classList.remove("active");
+  document.querySelector(".overlay").classList.remove("active");
 }
